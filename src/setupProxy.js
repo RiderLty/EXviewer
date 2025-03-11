860a2250a38d1f10387911d88a0ff8f148dd4cc6
DriverLin <lty6531600@gmail.com>
2025-03-11 19:10:16 +0800

    更新BUILD

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:7964',
+      target: 'http://192.168.3.3:7964',
       changeOrigin: true,
     })
   );
   app.use(
     '/websocket',
     createProxyMiddleware({
-      target: 'ws://localhost:7964',
+      target: 'ws://192.168.3.3:7964',
       changeOrigin: true,
       ws: true,
     })
